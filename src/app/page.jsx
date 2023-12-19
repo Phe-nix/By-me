54266495663881c19097aca6b7216afbd124ea0d
Phe-nix <88503925+Phe-nix@users.noreply.github.com>
2023-12-20 01:45:56 +0700

    🚧 On progress

---
+++ src/app/page.jsx
@@ -0,0 +1,17 @@
+
+export default function Home() {
+  return (
+    <main className="flex-1">
+        <section className="w-full py-6 sm:py-12 md:py-24 lg:py-32 xl:py-48">
+          <div className="container px-4 text-center">
+            <h1 className="text-4xl font-bold tracking-tighter sm:text-5xl xl:text-6xl ">
+              Welcome to my Portfolio
+            </h1>
+            <p className="max-w-[600px] text-gray-500 mt-4 dark:text-gray-400">
+              My all projects and experiences are here.
+            </p>
+          </div>
+        </section>
+    </main>
+  )
+}
